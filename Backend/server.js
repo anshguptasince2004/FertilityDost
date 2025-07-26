a93ae316fd059eb5e541f3f332b03b9c4da4a808
Ansh Gupta <anshgupta3221@gmail.com>
2025-07-26 10:10:03 +0530

    organised database schema

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/enroll", enrollRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
