e8a1473c507e911a53bb1e7612238fcb35e6bf2d
Ansh Gupta <anshgupta3221@gmail.com>
2025-07-27 22:01:09 +0530

    Community in Programs

--- src/pages/Programs/CommunitySection.jsx
+++ src/pages/Programs/CommunitySection.jsx
@@ -1,54 +1,60 @@
-import React, { useState } from "react";
-import testimonialImg from "../../assets/Programs/h5.png";
-import clientImg from "../../assets/Programs/docter1.png";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import testimonialImg1 from "../../assets/Programs/h1.png";
+import testimonialImg2 from "../../assets/Programs/h2.png";
+import testimonialImg3 from "../../assets/Programs/h3.png";
+import testimonialImg4 from "../../assets/Programs/h4.png";
+import clientImg1 from "../../assets/Programs/docter1.png";
+import clientImg2 from "../../assets/Programs/docter2.png";
+import clientImg3 from "../../assets/Programs/docter3.png";
+import clientImg4 from "../../assets/Programs/docter1.png";
 
 const testimonials = [
   {
     name: "Mrs. Shivani Arora, Gurugram",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.",
-    img: clientImg,
-    video: testimonialImg,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+    img: clientImg1,
+    video: testimonialImg1,
   },
   {
     name: "Mrs. Shivani Arora1, Gurugram",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.",
-    img: clientImg,
-    video: testimonialImg,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+    img: clientImg2,
+    video: testimonialImg2,
   },
   {
     name: "Mrs. Shivani Arora2, Gurugram",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.",
-    img: clientImg,
-    video: testimonialImg,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+    img: clientImg3,
+    video: testimonialImg3,
   },
   {
     name: "Mrs. Shivani Arora3, Gurugram",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.",
-    img: clientImg,
-    video: testimonialImg,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+    img: clientImg4,
+    video: testimonialImg4,
   },
 ];
 
 const CommunitySection = () => {
   const [current, setCurrent] = useState(0);
 
-  const nextSlide = () => setCurrent((prev) => (prev + 1) % testimonials.length);
-  const prevSlide = () => setCurrent((prev) => (prev - 1 + testimonials.length) % testimonials.length);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % testimonials.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <section className="community-section container">
       <div className="community-header">
         <h2>
-          Hear From Our<span className="highlight"><br/>Community</span>
+          Hear From Our<span className="highlight"><br />Community</span>
         </h2>
       </div>
 
       <div className="carousel-wrapper">
-        <button className="nav-arrow left" onClick={prevSlide}>
-          <FaChevronLeft />
-        </button>
-
         <div className="community-content fade-in">
           <div className="testimonial-card">
             <div className="quote-bg">❝</div>
@@ -67,10 +73,6 @@ const CommunitySection = () => {
             />
           </div>
         </div>
-
-        <button className="nav-arrow right" onClick={nextSlide}>
-          <FaChevronRight />
-        </button>
       </div>
 
       <div className="testimonial-dots">
